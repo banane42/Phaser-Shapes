43cc48318c7fdb288e500a0267546a17dd19af0c
gjmorris <gjmorris@iu.edu>
2020-02-25 00:14:50 -0500

    Worked on menu

---
+++ Phaser Shapes/Assets/Scripts/MenuController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject[] shapePrefabs;
+    public Text Title;
+
+    int titleResizeDelta = 1;
+    float spawnTime = 1f;
+    float ellapsedTime = 0f;
+
+    private void Update() {
+
+        ellapsedTime += Time.deltaTime;
+        ResizeTitle();
+
+        if (ellapsedTime >= spawnTime) {
+
+            YeetShape();
+            ellapsedTime = 0f;
+            spawnTime = Random.value * 4f;
+
+        }
+
+    }
+
+    void ResizeTitle() {
+
+        if (Title.fontSize > 55) {
+            titleResizeDelta = -1;
+        }
+        else if (Title.fontSize < 30) {
+            titleResizeDelta = 1;
+        }
+
+        Title.fontSize += titleResizeDelta;
+
+    }
+
+    void YeetShape() {
+
+        int shapeIndex = Random.Range(0, shapePrefabs.Length);
+
+        float xPos = Random.Range(0f, Camera.main.pixelWidth);
+        float yPos = Random.Range(0f, Camera.main.pixelHeight);
+
+        Vector3 yeetVector = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 1f);
+
+        Vector3 locationVector = Camera.main.ScreenToWorldPoint(new Vector3(xPos, yPos, 10f));
+
+        Vector3 spawnPoint = locationVector + (yeetVector.normalized * 25f);
+
+        Instantiate(shapePrefabs[shapeIndex], spawnPoint, Quaternion.identity).GetComponent<MenuShapeController>().Instantiate(spawnPoint);
+                
+
+    }
+
+}
---
+++ Phaser Shapes/Assets/Scripts/MenuShapeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShapeController : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    float lifetime = 0f;
+    float rotationAngle = 0f;
+    Vector3 moveVector = Vector3.one;
+
+    private void Awake() {
+
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.color = new Color(Random.value * 255, Random.value * 255, Random.value * 255);
+
+        rotationAngle = Random.value * 10f;
+
+    }
+
+    private void Update() {
+
+        lifetime += Time.deltaTime;
+
+        if (lifetime > 10f) {
+            //Destroy(gameObject);
+        }
+
+        transform.Rotate(new Vector3(0f, 0f, rotationAngle), Space.Self);
+        //transform.Translate(moveVector, Space.World);
+
+    }
+
+    public void Instantiate(Vector3 mv) {
+
+        moveVector = mv;
+
+    }
+
+}
